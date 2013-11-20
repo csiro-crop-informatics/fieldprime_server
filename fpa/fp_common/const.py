0da3f644c16aa3715533443de683020bf2c84075
kir145 <kir145@csiro.au>
2013-11-20 23:39:29 +0000

    added const.py in fp_common

---
+++ fpa/fp_common/const.py
@@ -0,0 +1,22 @@
+#
+# const.py
+# Michael Kirk 2013
+#
+# Constants (eg database table and field names).
+#
+
+
+# Table trialUnitAttribute:
+TABLE_TUA = "trialUnitAttribute"
+TUA_ID = "id"
+TUA_TRIAL_ID = "trial_id"
+TUA_NAME = "name"
+TUA_DATATYPE = "datatype"
+TUA_FUNC = "func"
+
+
+# Table attributeValue:
+TABLE_ATTRIBUTE_VALUES = "attributeValue"
+AV_TUA_ID = "trialUnitAttribute_id"
+AV_TRIAL_UNIT = "trialUnit_id"
+AV_VALUE = "value"
