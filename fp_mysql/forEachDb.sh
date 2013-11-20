9a341c368f2e6cab1fa8c4c641d802fffdc145e3
kir145 <kir145@csiro.au>
2013-11-20 23:17:18 +0000

    script update

--- fp_mysql/forEachDb.sh
+++ fp_mysql/forEachDb.sh
@@ -1,38 +1,46 @@
 #!/bin/bash
+#
+# Retrieves the list of names of  fp_* databases, and then calls
+# specified command for each one (with variable $DBNAME set to the
+# database name).  The command can be mysql, from the command line
+# or a file, or a bash command from the command line.
+# EG:
+#   ./forEachDb.sh -b 'echo $DBNAME'     # Note single quotes else use \$DBNAME
+#   ./forEachDb.sh -e 'select count(*) as numTrials from trial'
+#   ./forEachDb.sh -f mySqlScript.sql
+#
  
 usage() {
-  echo "Usage: $0 -e <mysql command string> | -f <mysql command file>" 1>&2
+  echo "Usage: $0 -e <mysql command string> | -f <mysql command file> | -b <bash command>" 1>&2
   echo "  (and you will need Superadm password)" 1>&2
   exit 1;
 }
 
-if [ -n "$CMD" ] && [ -n "$INFILE" ]; then
-  echo "jjjjjToo many parameters"
-  usage
-fi
-
-while getopts "f:e:" o; do
+numOpt=0
+while getopts "f:e:b:" o; do
     case "${o}" in
         f)
             INFILE=${OPTARG}
+            ((numOpt++))
             ;;
         e)
             CMD=${OPTARG}
+            ((numOpt++))
+           ;;
+        b)
+            BCMD=${OPTARG}
+            ((numOpt++))
             ;;
         *)
             usage
             ;;
     esac
 done
+if [ $numOpt -ne 1 ]; then
+  echo "Wrong number of parameters"
+  usage
+fi
 
-if [ -n "$CMD" ] && [ -n "$INFILE" ]; then
-  echo "Too many parameters"
-  usage
-fi
-if [ -z "$CMD" ] && [ -z "$INFILE" ]; then
-  echo "Not enough parameters"
-  usage
-fi
 
 echo "Please enter password"
 read -s DBPW
@@ -40,11 +48,14 @@ MYSQL=`which mysql`
 DATABASES=`mysql -BN  -uSuperadm -p$DBPW -e"show databases"`
 for DBNAME in $DATABASES; do if [[ $DBNAME == fp_* ]];
 then
-  echo $DBNAME
+  echo $DBNAME:
   if [ -n "$CMD" ]; then
     export MCMD="$MYSQL -uSuperadm -p$DBPW $DBNAME -e \"$CMD\""
-  else
+  elif [ -n "$INFILE" ]
+  then
     export MCMD="$MYSQL -uSuperadm -p$DBPW $DBNAME  < $INFILE"
+  else
+    export MCMD=$BCMD
   fi
   eval $MCMD
 fi
