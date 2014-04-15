1843113e9b134d5ae4d893e0ff2b44b5c10fcbf1
kir145 <kir145@csiro.au>
2014-04-15 01:20:56 +0000

    x

--- fp_mysql/new_fprime_user.sh
+++ fp_mysql/new_fprime_user.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
  
-EXPECTED_ARGS=2
+EXPECTED_ARGS=4
 E_BADARGS=65
 MYSQL=`which mysql`
  
  
 if [ $# -ne $EXPECTED_ARGS ]
 then
-  echo "Usage: $0 name password (and you will need Superadm password)"
+  echo "Usage: $0 name password contactName contactEmail"
+  echo "  You will need super password."
   exit $E_BADARGS
 fi
 
@@ -15,6 +16,8 @@ WEBUSER=fpwserver
 DBNAME=fp_$1
 DBUSER=fp_$1
 DBPASS=$2 
+CONTACT_NAME=$3
+CONTACT_EMAIL=$4
 
 
 $MYSQL -uSuperadm -p <<EOF
@@ -24,5 +27,6 @@ source fprime.create.tables.sql;
 grant all on $DBNAME.* to '$DBUSER'@'localhost' identified by '$DBPASS';
 grant all on $DBNAME.* to '$WEBUSER'@'localhost';
 flush privileges;
+insert system (name, value) values ('contactName', '$CONTACT_NAME'), ('contactEmail', '$CONTACT_EMAIL')
 EOF
 
