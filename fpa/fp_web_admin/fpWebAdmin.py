935b1bb65a20b49c1cc32d2500a58f4261215674
kir145 <kir145@csiro.au>
2014-08-25 22:36:21 +0000

    x

--- fpa/fp_web_admin/fpWebAdmin.py
+++ fpa/fp_web_admin/fpWebAdmin.py
@@ -323,9 +323,93 @@ class htmlChunkSet:
             h += '\n'
         return h
 
+    def htmlTabs(self):
+        h = '''
+        <script>
+        $(document).ready(init)
+
+        var tabLinks = new Array();
+        var contentDivs = new Array();
+
+    function init() {
+
+      // Grab the tab links and content divs from the page
+      var tabListItems = document.getElementById('tabs').childNodes;
+      for ( var i = 0; i < tabListItems.length; i++ ) {
+        if ( tabListItems[i].nodeName == "LI" ) {
+          var tabLink = getFirstChildWithTagName( tabListItems[i], 'A' );
+          var id = getHash( tabLink.getAttribute('href') );
+          tabLinks[id] = tabLink;
+          contentDivs[id] = document.getElementById( id );
+        }
+      }
+
+      // Assign onclick events to the tab links, and
+      // highlight the first tab
+      var i = 0;
+
+      for ( var id in tabLinks ) {
+        tabLinks[id].onclick = showTab;
+        tabLinks[id].onfocus = function() { this.blur() };
+        if ( i == 0 ) tabLinks[id].className = 'selected';
+        i++;
+      }
+
+      // Hide all content divs except the first
+      var i = 0;
+
+      for ( var id in contentDivs ) {
+        if ( i != 0 ) contentDivs[id].className = 'tabContent hide';
+        i++;
+      }
+    }
+
+    function showTab() {
+      var selectedId = getHash( this.getAttribute('href') );
+
+      // Highlight the selected tab, and dim all others.
+      // Also show the selected content div, and hide all others.
+      for ( var id in contentDivs ) {
+        if ( id == selectedId ) {
+          tabLinks[id].className = 'selected';
+          contentDivs[id].className = 'tabContent';
+        } else {
+          tabLinks[id].className = '';
+          contentDivs[id].className = 'tabContent hide';
+        }
+      }
+
+      // Stop the browser following the link
+      return false;
+    }
+
+    function getFirstChildWithTagName( element, tagName ) {
+      for ( var i = 0; i < element.childNodes.length; i++ ) {
+        if ( element.childNodes[i].nodeName == tagName ) return element.childNodes[i];
+      }
+    }
+
+    function getHash( url ) {
+      var hashPos = url.lastIndexOf ( '#' );
+      return url.substring( hashPos + 1 );
+    }
+    </script>
+        '''
+        # Tab headers:
+        h += '<ul id="tabs">\n'
+        for c in self.chunks:
+            h += '  <li><a href="#{0}">{1}</a></li>\n'.format(c[0], c[1])
+        h += '</ul>\n'
+
+        # Tab content divs:
+        for c in self.chunks:
+            h += '<div class="tabContent" id="{0}">\n{1}\n</div>\n\n'.format(c[0], c[2])
+        return h
+
 def TrialHtml(sess, trialId):
 #-----------------------------------------------------------------------
 # Returns the HTML for a top level page to display/manage a given trial.
+# Or None if no trial with given id.
 #
     trial = dbUtil.GetTrial(sess, trialId)
     if trial is None: return None
@@ -335,7 +419,7 @@ def TrialHtml(sess, trialId):
     hts.addChunk('traits', 'Traits', htmlTrialTraits(sess, trial))
     hts.addChunk('scoresets', 'Score Sets', htmlTrialScoreSets(sess, trialId))
     hts.addChunk('data', 'Score Data', htmlTrialData(sess, trial))
-    return hts.html()
+    return hts.htmlTabs()
 
 
 def OldTrialHtml(sess, trialId):
