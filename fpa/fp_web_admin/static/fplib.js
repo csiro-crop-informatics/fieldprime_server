58a93bbe5bcee81c40eefe1be61830eca1771129
kir145 <kir145@csiro.au>
2014-09-12 07:14:32 +0000

    x

--- fpa/fp_web_admin/static/fplib.js
+++ fpa/fp_web_admin/static/fplib.js
@@ -11,42 +11,32 @@ var fplib = {};
  * parameter tabListId should be the id of a UL element containing links
  * for the desired tabs. Each LI should be an anchor for the relevant div
  * on the page.
- * Note there is a section in fpStyle.css for styling the tabs. Unfortunately
- * It seems to be assuming that tabListId is "tabs".
- * Perhaps we should be adding the style directly in this func..
-
- something like:
-
-var css='table td:hover{background-color:#00ff00}';
-style=document.createElement('style');
-if (style.styleSheet)
-    style.styleSheet.cssText=css;
-else
-    style.appendChild(document.createTextNode(css));
-document.getElementsByTagName('head')[0].appendChild(style);
-
-
+ * Note all the styling is done in this function. There is no separate css
+ * required.
  */
 fplib.initTabs = function (tabListId) {
     var tabLinks = {};
     var contentDivs = {};
+    var STORAGE_TAG = "fpCurrTrialPageTab"
 
     function setTab(tabId) {
       // store tabId in session storage for retrieval on navigation back to page:
       if (window.sessionStorage){
-        sessionStorage.setItem("currTrialPageTab", tabId);
+        sessionStorage.setItem(STORAGE_TAG, tabId);
       }
 
       // Highlight the selected tab, and dim all others.
       // Also show the selected content div, and hide all others.
       for (var id in contentDivs) {
         if (id == tabId) {
-          tabLinks[id].className = 'selected';
-          contentDivs[id].className = 'tabContent';
+          //tabLinks[id].className = 'selected';
+          contentDivs[id].style.display = "";
+
         } else {
-          tabLinks[id].className = '';
-          contentDivs[id].className = 'tabContent hide';
+          //tabLinks[id].className = '';
+          contentDivs[id].style.display = "none";
         }
+        styleTabLink(tabLinks[id], id === tabId);
       }
 
     }
@@ -68,19 +58,50 @@ fplib.initTabs = function (tabListId) {
       return url.substring(hashPos + 1);
     }
 
+    function styleTabLink(tabLink, selected) {
+        tabLink.style.fontWeight = selected ? 'bold' : '';
+        tabLink.style.color = selected ? '#000' : '#42454a';
+        tabLink.style.backgroundColor = selected ? '#f1f0ee' : 'pink';
+        tabLink.style.padding = selected ? '0.7em 0.3em 0.38em 0.3em' : '0.3em';
+
+        tabLink.style.minWidth = '120px';
+        tabLink.style.display = 'block';
+        tabLink.style.border = '1px solid #c9c3ba';
+        tabLink.style.borderBottom = 'none';
+        tabLink.style.textDecoration = 'none';
+        tabLink.style.margin = '0';
+    }
+
     //-- Code: ------------------------------------------------------
     var i;
     var id;
 
-    // Get the tab links and content divs from the page
-    var tabListItems = document.getElementById(tabListId).childNodes;
+    // Get the tab list element and style it:
+    var tabListElement = document.getElementById(tabListId);
+    tabListElement.style.listStyleType = 'none';
+    tabListElement.style.whiteSpace = 'nowrap';
+    tabListElement.style.margin = '30px 0 0 0';
+    tabListElement.style.padding = '0 0 0.3em 0';
+
+    // Process the children:
+    var tabListItems = tabListElement.childNodes;
     for (i = 0; i < tabListItems.length; i++ ) {
       if (tabListItems[i].nodeName == "LI") {
+        // style the li:
+        tabListItems[i].style.display = 'inline-block';
+        tabListItems[i].style.margin = '0';
+
         var tabLink = getFirstChildWithTagName(tabListItems[i], 'A');
-        //tabLink.style.backgroundColor = "red";    // example of setting style..
         id = getHash(tabLink.getAttribute('href'));
         tabLinks[id] = tabLink;
-        contentDivs[id] = document.getElementById(id);
+        styleTabLink(tabLink, false);
+
+        // Record and style the content chunk:
+        var contChunk = document.getElementById(id);
+        contentDivs[id] = contChunk;
+        contChunk.style.border = "1px solid #c9c3ba";
+        contChunk.style.padding = "0.5em";
+
       }
     }
 
@@ -93,7 +114,7 @@ fplib.initTabs = function (tabListId) {
 
     var currTab;
     if (window.sessionStorage) {
-      currTab=sessionStorage.getItem("currTrialPageTab");
+      currTab=sessionStorage.getItem(STORAGE_TAG);
       fplib.currTab = currTab;
     }
     setTab(currTab);
