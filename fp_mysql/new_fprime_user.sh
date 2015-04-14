fd6980b560c349f9c74204ed8962be38e6eb25eb
kir145 <kir145@csiro.au>
2015-04-14 03:55:23 +0000

    x

--- fp_mysql/new_fprime_user.sh
+++ fp_mysql/new_fprime_user.sh
@@ -13,8 +13,9 @@ then
 fi
 
 WEBUSER=fpwserver
-DBNAME=fp_$1
-DBUSER=fp_$1
+PROJNAME=$1
+DBNAME=fp_$PROJNAME
+DBUSER=fp_$PROJNAME
 DBPASS=$2 
 CONTACT_NAME=$3
 CONTACT_EMAIL=$4
@@ -26,6 +27,8 @@ use $DBNAME;
 source fprime.create.tables.sql;
 grant all on $DBNAME.* to '$DBUSER'@'localhost' identified by '$DBPASS';
 grant all on $DBNAME.* to '$WEBUSER'@'localhost';
+insert project values (2, null, '$PROJNAME', '$CONTACT_NAME', 'CONTACT_EMAIL');
+insert fpsys.project values (null, '$PROJNAME', '$DBNAME');
 flush privileges;
 insert system (name, value) values ('contactName', '$CONTACT_NAME'), ('contactEmail', '$CONTACT_EMAIL')
 EOF
