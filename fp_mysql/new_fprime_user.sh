757a6f56a317fa49f0cf7e025f5f877fac8fadbe
kir145 <kir145@csiro.au>
2015-11-02 04:45:18 +0000

    x

--- fp_mysql/new_fprime_user.sh
+++ fp_mysql/new_fprime_user.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
  
-EXPECTED_ARGS=4
+EXPECTED_ARGS=3
 E_BADARGS=65
 MYSQL=`which mysql`
  
- 
+while getopts "p:" opt; do
+case $opt in
+    p)
+      DBPASS=$OPTARG
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
+shift $(expr $OPTIND - 1 )
+
+# Show usage if wrong number of args: 
 if [ $# -ne $EXPECTED_ARGS ]
 then
-  echo "Usage: $0 name password contactName contactEmail"
-  echo "  You will need super password."
+  echo "Usage: $0 name [-p password] projectName contactName contactEmail"
+  echo "  You will need to provide mysql super password."
+  echo '  If -p password is present, then a FieldPrime user is created (with that password).'
   exit $E_BADARGS
 fi
 
@@ -16,17 +29,22 @@ WEBUSER=fpwserver
 PROJNAME=$1
 DBNAME=fp_$PROJNAME
 DBUSER=fp_$PROJNAME
-DBPASS=$2 
 CONTACT_NAME=$3
 CONTACT_EMAIL=$4
 
+# Mysql command to create user if specified:
+if [ -n ${DBPASS+x} ]
+then
+    CREATE_USER_CMD="grant all on $DBNAME.* to '$DBUSER'@'localhost' identified by '$DBPASS';"
+fi
 
+# Run Mysql script:
 $MYSQL -uSuperadm -p <<EOF
 create database if not exists $DBNAME;
 use $DBNAME;
 source fprime.create.tables.sql;
-# Comment out next line if non ***REMOVED*** login not required.
-grant all on $DBNAME.* to '$DBUSER'@'localhost' identified by '$DBPASS';
+# Create mysql user if password provided:
+$CREATE_USER_CMD
 grant all on $DBNAME.* to '$WEBUSER'@'localhost';
 insert project values (2, null, '$PROJNAME', '$CONTACT_NAME', '$CONTACT_EMAIL');
 insert fpsys.project values (null, '$PROJNAME', '$DBNAME');
