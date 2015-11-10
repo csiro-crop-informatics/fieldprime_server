6a0a0b0022036ae6babf4217ffba9ba50e35b33f
kir145 <kir145@csiro.au>
2015-11-10 06:36:05 +0000

    x

--- fpa/fp_web_admin/static/newTrait.js
+++ fpa/fp_web_admin/static/newTrait.js
@@ -23,169 +23,101 @@ fpTrait.createFieldset = function(legendText) {
 };
 
 
-/*
- * _addRow()
- * Add row to catTable.
- */
-fpTrait._addRow = function(){
-    var rootDiv = this.topDiv;
-    var row=rootDiv.catTable.insertRow(-1);
+fpTrait._addRow = function(rootDiv, preset, isNew) {
+    if (isNew)
+        preset = {caption:"", value:"", imageURL:"None"};
+    var tab = rootDiv.catTable;
+    var drow = tab.insertRow(-1);
+    var index = rootDiv.catCount;
 
-    // Add caption, value, and imageFile fields:
-    var names = ['caption_', 'value_', 'imgfile_'];
-    for (var i = 0; i < names.length; i++) {
-        var cap = document.createElement("input");
-        cap.type = (i == 2) ? "file" : "text";
-        cap.id = names[i] + rootDiv.catCount;
-        cap.name = cap.id;
-        row.insertCell(-1).appendChild(cap);
+    // Caption:
+    var capcell = drow.insertCell(-1);
+    var capInput = document.createElement('input');
+    capInput.type = 'text';
+    capInput.id = 'caption_' + index;
+    capInput.name = capInput.id;
+    capInput.value = preset.caption;
+    capcell.appendChild(capInput);
+
+    // Value field:
+    var val = document.createElement("input");
+    val.type = "text";
+    val.id = 'value_' + index;
+    val.value = preset.value;
+    val.name = 'value_' + index;
+    val.readOnly = !isNew;
+    drow.insertCell(-1).appendChild(val);
+
+    // Image URL:
+    var icel = drow.insertCell(-1);
+    if (preset.imageURL === 'None') {
+        icel.appendChild(document.createTextNode("\u00A0No Image\u00A0"));  // \u00A0
+    } else {
+        var anc = document.createElement("A");
+        anc.href = preset.imageURL;
+        anc.appendChild(document.createTextNode('\u00A0Image set\u00A0'));
+        icel.appendChild(anc);
     }
+    icel = drow.insertCell(-1);
+    var fcel = document.createElement("input");
+    fcel.type = 'file';
+    fcel.id = 'imgfile_' + index;
+    fcel.name = 'imgfile_' + index;
+    fcel.style.display = 'inline';
+    icel.appendChild(fcel);
 
-    // Add delete button, could put it in cell, but doesn't seem to be necessary
-    var btn=document.createElement("BUTTON");
-    btn.appendChild(document.createTextNode("-"));
-    btn.onclick = function() {
-        btn.parentNode.parentNode.removeChild(btn.parentNode);
-        --rootDiv.catCount;
-    };
-    row.appendChild(btn);
-
+    // New cell needs delete button:
+    if (isNew && index > 0) {
+        var btn=document.createElement("BUTTON");
+        btn.appendChild(document.createTextNode("-"));
+        btn.onclick = function() {
+            btn.parentNode.parentNode.removeChild(btn.parentNode);
+            --rootDiv.catCount;
+        };
+        drow.appendChild(btn);
+    }
     ++rootDiv.catCount;
-    return false;  // need this else browser leaves the page
 };
 
 /*
  * categoryTraitFormElement()
- * Adds to newDiv a fieldset containing table of category trait elements.
+ * Adds to rootDiv a fieldset containing table of category trait elements.
  * presets are existing values to put in the table.
  */
-fpTrait.categoryTraitFormElement = function(newDiv, presets) {
-    var fset = newDiv.appendChild(fpTrait.createFieldset('Categories'));
+fpTrait.categoryTraitFormElement = function(rootDiv, presets) {
+    var fset = rootDiv.appendChild(fpTrait.createFieldset('Categories'));
     var tab = fset.appendChild(document.createElement('table'));
-    tab.style.width = "766";
-    tab.style.padding = "0";
     tab.id = "catTable";
     tab.style.border="0";
 
-    // Store the count and the table in the div, for access by other functions:
-    newDiv.catCount = (presets !== undefined) ? presets.length : 1;
-    newDiv.catTable = tab;
-
     // Table header:
     var hrow = tab.createTHead().insertRow(-1);
-    hrow.insertCell(-1).innerHTML = "Caption";   // use document.createTextNode("Caption");
+    hrow.insertCell(-1).innerHTML = "Caption";
     hrow.insertCell(-1).innerHTML = "Value";
-    hrow.insertCell(-1).appendChild(document.createTextNode("Image File"));
-    if (true) {
-        var btn = document.createElement("BUTTON");
-        btn.name = "button";
-        btn.innerHTML = "+";
-        btn.onclick = fpTrait._addRow;
-        btn.topDiv = newDiv;
-        hrow.insertCell(-1).appendChild(btn);
-    } else {
-        hrow.insertCell(-1).innerHTML = '<input name="button" type="button" value="+" onclick="fpTrait._addRow()">';
-    }
+    hrow.insertCell(-1).appendChild(document.createTextNode("Image"));
+    hrow.insertCell(-1).appendChild(document.createTextNode(""));
+    var btn = document.createElement("BUTTON");
+    btn.name = "button";
+    btn.innerHTML = "+";
+    btn.onclick = function(){
+        fpTrait._addRow(rootDiv, null, true);
+        return false;  // need this else browser leaves the page
+    };
+    btn.topDiv = rootDiv;
+    hrow.insertCell(-1).appendChild(btn);
+
+    // Store the count and the table in the div, for access by other functions:
+    rootDiv.catCount = 0;  //(presets !== undefined) ? presets.length : 1;
+    rootDiv.catTable = tab;
 
     // Table contents:
-    var drow;
-    var dcell;
-    var inp;
     if (presets !== undefined) {
         // add rows for existing categories (with no remove button):
         for (var i = 0; i < presets.length; i++) {
-            drow = tab.insertRow(-1);
-
-            // Caption:
-            var capcell = drow.insertCell(-1);
-            capcell.style.width = '191';
-            //capcell.innerHTML = '<input type="text" id="caption_' + i + '" value="'
-            // + presets[i].caption + '" name="caption_' + i + '"/>';
-            var capInput = document.createElement('input');
-            capInput.type = 'text';
-            capInput.id = 'caption_' + i;
-            capInput.name = capInput.id;
-            capInput.value = presets[i].caption;
-            capcell.appendChild(capInput);
-
-            // Value field:
-            var val = document.createElement("input");
-            val.type = "text";
-            val.id = 'value_' + i;
-            val.value = presets[i].value;
-            val.name = 'value_' + i;
-            val.readOnly = true;
-            drow.insertCell(-1).appendChild(val);
-
-            // Image URL:
-            var icel = drow.insertCell(-1);
-            icel.style.width = '191';
-            icel.style.whiteSpace = 'nowrap';
-            if (presets[i].imageURL === 'None') {
-                icel.appendChild(document.createTextNode("No Image"));
-            } else {
-                var anc = document.createElement("A");
-                anc.href = presets[i].imageURL;
-                anc.appendChild(document.createTextNode('Image set'));
-                icel.appendChild(anc);
-            }
-            var fcel = document.createElement("input");
-            fcel.type = 'file';
-            fcel.id = 'imgfile_' + i;
-            fcel.name = 'imgfile_' + i;
-            icel.appendChild(fcel);
-/* This an attempt to stop the two elements (text node and file input) in this td from wrapping, unsuccessful so far..
-            // Image URL:
-            var icel = drow.insertCell(-1);
-            icel.style.width = '191';
-            icel.style.whiteSpace = 'nowrap';
-            icel.style.display = 'inline-block';
-            var bdiv = document.createElement("div");
-            bdiv.style.display = 'inline';
-            bdiv.style.whiteSpace = 'nowrap';
-            //bdiv.style.overflow = 'scroll';
-            if (presets[i].imageURL === 'None') {
-                bdiv.appendChild(document.createTextNode("No Image"));
-            } else {
-                var anc = document.createElement("A");
-                anc.href = presets[i].imageURL;
-                anc.appendChild(document.createTextNode('Image set'));
-                bdiv.appendChild(anc);
-            }
-            var fcel = document.createElement("input");
-            fcel.type = 'file';
-            fcel.id = 'imgfile_' + i;
-            fcel.name = 'imgfile_' + i;
-            bdiv.appendChild(fcel);
-            icel.appendChild(bdiv);
-*/
+            fpTrait._addRow(rootDiv, presets[i], false);
         }
     } else {
-        // Set up single initial row:
-        drow = tab.insertRow(-1);
-
-        // Caption:
-        dcell = drow.insertCell(-1);
-        dcell.style.width = '191';
-        inp = document.createElement('input');
-        inp.type = 'text';
-        inp.id = 'caption_0';
-        inp.name = inp.id;
-        dcell.appendChild(inp);
-
-        // Value field:
-        inp = document.createElement("input");
-        inp.type = "text";
-        inp.id = 'value_0';
-        inp.name = inp.id;
-        drow.insertCell(-1).appendChild(inp);
-
-        // Image URL:
-        inp = document.createElement("input");
-        inp.type = "file";
-        inp.id = 'imgfile_0';
-        inp.name = inp.id;
-        drow.insertCell(-1).appendChild(inp);
+        fpTrait._addRow(rootDiv, null, true);
     }
 };
 
@@ -266,12 +198,12 @@ fpTrait.validateTraitTypeSpecific = function(divName, traitType)
         for (var i=0; i < parseInt(rootDiv.catCount, 10); i++) {
             var catCap = document.getElementById("caption_" + i).value;
             if (catCap !== null && catCap === "") {
-                alert("Please provide a caption for row " + i);
+                alert("Please provide a caption for row " + (i + 1));
                 return false;
             }
             var catVal  = document.getElementById("value_" + i).value;
             if (catVal !== null && catVal === "") {
-                alert("Please provide a value for row " + i);
+                alert("Please provide a value for row " + (i+1));
                 return false;
             }
         }
