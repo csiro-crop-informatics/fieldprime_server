ee00207b471e47802cc8b0f10dfc084944f342fa
kir145 <kir145@csiro.au>
2015-12-11 06:06:13 +0000

    x

--- fpa/fp_web_admin/stats.py
+++ fpa/fp_web_admin/stats.py
@@ -4,36 +4,38 @@
 # NB Environment var MPLCONFIGDIR must be set before importing this module.
 # It gives a folder for matplotlib stuff, must be writeable by web server.
 #
+# No longer used - we want to do graphic on client, not server, and matplotlib
+# requires a heap of other stuff to be installed. Delete this file when happy.
 
-import urllib
-import StringIO
-import flask
-
-import os
-#os.environ['MPLCONFIGDIR'] = '***REMOVED***/matplotlib/'
-#os.environ['MPLCONFIGDIR'] = flask.current_app.config['MPLCONFIGDIR']
-#import os
-#import tempfile
-#os.environ['MPLCONFIGDIR'] = tempfile.mkdtemp()
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from matplotlib.figure import Figure
-
-
-def htmlBoxplot(data):
-#-------------------------------------------------------------------------------
-# Parameter data should be an array of Datum, from a single traitInstance,
-# of numerical type.
+# import urllib
+# import StringIO
+# import flask
+#
+# import os
+# #os.environ['MPLCONFIGDIR'] = '***REMOVED***/matplotlib/'
+# #os.environ['MPLCONFIGDIR'] = flask.current_app.config['MPLCONFIGDIR']
+# #import os
+# #import tempfile
+# #os.environ['MPLCONFIGDIR'] = tempfile.mkdtemp()
+# from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+# from matplotlib.figure import Figure
 #
 #
-    fig=Figure()
-    ax=fig.add_subplot(111)
-    x=[]
-    for datum in data:
-        if not datum.isNA():
-            x.append(float(datum.getValue()))
-    ax.boxplot(x, vert=False)
-    canvas=FigureCanvas(fig)
-    png_output = StringIO.StringIO()
-    canvas.print_png(png_output)
-    png_output = png_output.getvalue().encode("base64")
-    return '<img src="data:image/png;base64,{0}"/>'.format(urllib.quote(png_output.rstrip('\n')))
+# def htmlBoxplot(data):
+# #-------------------------------------------------------------------------------
+# # Parameter data should be an array of Datum, from a single traitInstance,
+# # of numerical type.
+# #
+# #
+#     fig=Figure()
+#     ax=fig.add_subplot(111)
+#     x=[]
+#     for datum in data:
+#         if not datum.isNA():
+#             x.append(float(datum.getValue()))
+#     ax.boxplot(x, vert=False)
+#     canvas=FigureCanvas(fig)
+#     png_output = StringIO.StringIO()
+#     canvas.print_png(png_output)
+#     png_output = png_output.getvalue().encode("base64")
+#     return '<img src="data:image/png;base64,{0}"/>'.format(urllib.quote(png_output.rstrip('\n')))
