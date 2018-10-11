536ec2d8612b05cb9f619cfecdf434093bb929ea
Tim Erwin <erw001@snivy-cl.nexus.csiro.au>
2018-10-11 16:44:48 +1100

    Update to work with docker config

--- fpa/fp_common/config.py
+++ fpa/fp_common/config.py
@@ -15,8 +15,8 @@ from default_config import *
 # Development Server
 #
 DEBUG = True                                                # Flask debug mode
-FP_ROOT = os.environ.get('FP_ROOT','/opt/fprime/')          # Base path to application
-FP_LOG_DIR = FP_ROOT + 'fplog/'                             # Path to logs
-FP_LOG_FILE = FP_LOG_DIR + 'fp.log'                         # FP logfile
+FP_ROOT = os.environ.get('FIELDPRIME_ROOT','/app')          # Base path to application
+FP_LOG_DIR = FP_ROOT + '/fplog'                            # Path to logs
+FP_LOG_FILE = FP_LOG_DIR + '/fp.log'                         # FP logfile
 FP_LOG_LEVEL = logging.DEBUG                                # Logging level
 FP_FLAG_DIR = FP_LOG_DIR                                    # Path to file flag directory
