791f2b10b0a6bc6b989c033e104eed0ed995043c
Tim Erwin <erw001@snivy-cl.nexus.csiro.au>
2019-03-25 14:14:48 +1100

    Remove pagination and permissions

--- fieldprime-api/fieldprime/settings.py
+++ fieldprime-api/fieldprime/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_swagger',
     'fpapi',
 ]
 
@@ -78,7 +79,7 @@ WSGI_APPLICATION = 'fieldprime.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'fieldprime_test',
+        'NAME': 'fieldprime',
         'USER': 'root',
         'PASSWORD': 'foo',
         'HOST': '127.0.0.1',
@@ -114,12 +115,12 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        #'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
 
     # Pagination Settings
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    #'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    #'PAGE_SIZE': 10
 }
 
 # Internationalization
