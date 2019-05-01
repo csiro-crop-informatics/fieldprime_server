6a73601988319eec9614542e72ffd9837121eaeb
Tim Erwin <erw001@snivy-cl.nexus.csiro.au>
2019-05-01 16:36:44 +1000

    Code cleanup add non nested get views

--- fieldprime-api/fpapi/serializers.py
+++ fieldprime-api/fpapi/serializers.py
@@ -5,38 +5,6 @@ from fpapi import const as fpconst
 import logging
 logger = logging.getLogger(__name__)
 
-class NodeSerializer(serializers.ModelSerializer):
-
-    # Project is a required field (it is a foreign key) but when it is used inside a nested
-    # model creation this will be set when parent project is saved.
-
-    class Meta:
-        model = fpmodels.Node
-        fields = ("url","row", "col", "description", "barcode", "latitude", "longitude", "project", "trial")
-
-
-class NodeNestedSerializer(serializers.ModelSerializer):
-
-    # Project is a required field (it is a foreign key) but when it is used inside a nested
-    # model creation this will be set when parent project is saved.
-    project = serializers.PrimaryKeyRelatedField(required=False, queryset=fpmodels.Project.objects.all())
-    trial = serializers.PrimaryKeyRelatedField(required=False, queryset=fpmodels.Trial.objects.all())
-
-    class Meta:
-        model = fpmodels.Node
-        fields = ("url","row", "col", "description", "barcode", "latitude", "longitude", "project", "trial")
-
-class TrialSerializer(serializers.ModelSerializer):
-
-    #project = serializers.PrimaryKeyRelatedField(required=False, queryset=fpmodels.Project.objects.all())
-
-    class Meta:
-        model = fpmodels.Trial
-        fields = ("url", "name", "site", "year", "acronym", "uuid", "project")
-        extra_kwargs = {
-            'url': {'view_name': 'trial-detail-uuid', 'lookup_field': 'uuid'}
-        }
-
 class ChoicesField(serializers.Field):
     def __init__(self, choices, **kwargs):
         self._choices = choices
@@ -60,6 +28,69 @@ class ChoicesField(serializers.Field):
 class StringListField(serializers.ListField):
     child = serializers.CharField()
 
+class NodeSerializer(serializers.ModelSerializer):
+
+    # Project is a required field (it is a foreign key) but when it is used inside a nested
+    # model creation this will be set when parent project is saved.
+    # project = serializers.PrimaryKeyRelatedField(required=False, queryset=fpmodels.Project.objects.all())
+    # trial = serializers.PrimaryKeyRelatedField(required=False, queryset=fpmodels.Trial.objects.all())
+
+    class Meta:
+        model = fpmodels.Node
+        fields = ("url","row", "col", "description", "barcode", "latitude", "longitude")
+        extra_kwargs = {
+            'url': {'view_name': 'node-detail-uuid'}
+        }
+
+class NodeNestedSerializer(serializers.ModelSerializer):
+
+    # Project is a required field (it is a foreign key) but when it is used inside a nested
+    # model creation this will be set when parent project is saved.
+    project = serializers.PrimaryKeyRelatedField(required=False, queryset=fpmodels.Project.objects.all())
+    trial = serializers.PrimaryKeyRelatedField(required=False, queryset=fpmodels.Trial.objects.all())
+
+    class Meta:
+        model = fpmodels.Node
+        fields = ("url","row", "col", "description", "barcode", "latitude", "longitude", "project", "trial")
+
+class TrialSerializer(serializers.ModelSerializer):
+
+    #traits = serializers.SerializerMethodField('get_alternate_name')
+    #nodes = serializers.PrimaryKeyRelatedField(required=False, queryset=fpmodels.Node.objects.all())
+    """
+    nodes = serializers.HyperlinkedRelatedField(
+        view_name='node-detail-uuid',
+        lookup_field='id',
+        many=True,
+        read_only=True
+    )
+
+    _traits = serializers.HyperlinkedRelatedField(
+        view_name='trait-detail-uuid',
+        lookup_field='uuid',
+        many=True,
+        read_only=True
+    )
+    """
+    class Meta:
+        model = fpmodels.Trial
+        fields = ("url", "name", "site", "year", "acronym", "uuid", "project", "nodes", "_traits")
+        extra_kwargs = {
+            'url': {'view_name': 'trial-detail-uuid', 'lookup_field': 'uuid'}
+        }
+    
+    def get_alternate_name(self, obj):
+        return obj._traits
+
+class TraitSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = fpmodels.Trait
+        fields = ("url", "uuid", "caption", "description", "data_type")
+        extra_kwargs = {
+            'url': {'view_name': 'trait-detail-uuid', 'lookup_field': 'uuid'}
+        }
+
 class DataTypeSerializer(serializers.Serializer):
     """
         This serializer must be used with TraitNestedSerializer
@@ -78,6 +109,7 @@ class DataTypeSerializer(serializers.Serializer):
         Check that categorical data also has accepted_values
         """
         data = super(DataTypeSerializer, self).validate(data)
+        print("Data ", data)
         if data['data_type'] == fpconst.CATEGORICAL:
             if "accepted_values" not in data.keys():
                 raise serializers.ValidationError("Categorical data must provide accepted_values")
@@ -103,6 +135,7 @@ class TraitNestedSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
 
         data_type_data = validated_data.pop("data_type")
+        print("Data TYPE DATA", data_type_data)
         trial_id = validated_data.pop("trial")
 
         # For V2 api, all traits are public and reused with uuid
@@ -111,14 +144,18 @@ class TraitNestedSerializer(serializers.ModelSerializer):
 
         trait, created = fpmodels.Trait.objects.get_or_create(
             **validated_data, 
-            trial = -1, 
+            trial_id = -1, 
             _data_type=data_type_data["data_type"])
+        print("TRAITSAVED ",trait,trait.id,created)
 
         self._create_data_type(trial_id, trait, data_type_data)
         return trait
 
     def _create_data_type(self, trial_id, trait, data):
+        
         data_type = data.pop("data_type")
+        print("DATATYPE",data_type)
+
         # Handle numeric types
         if data_type in [fpconst.INTEGER, fpconst.DECIMAL]:
             trial_extra_data, created = fpmodels.TrialTraitNumeric.objects.get_or_create(
@@ -126,7 +163,7 @@ class TraitNestedSerializer(serializers.ModelSerializer):
                 trait = trait,
                 **data
             )
-            trial_extra_data.save()
+            #trial_extra_data.save()
             logger.debug("saved numeric info")
         # Handle categorical type
         elif data_type in [fpconst.CATEGORICAL]:        
@@ -137,7 +174,7 @@ class TraitNestedSerializer(serializers.ModelSerializer):
                     caption = category,
                     #TODO url...
                 )
-                trial_extra_data.save()
+                #trial_extra_data.save()
                 logger.debug("saved cat info: %s" % trial_extra_data)
         #TODO: Handle other types (date/photo/string)
 
@@ -153,9 +190,9 @@ class TrialNestedSerializer(serializers.ModelSerializer):
     class Meta:
         model = fpmodels.Trial
         fields = ("url", "name", "site", "year", "acronym", "uuid", "project", "traits", "nodes")
-        extra_kwargs = {
-            'url': {'view_name': 'trial-detail-uuid', 'lookup_field': 'uuid'}
-        }
+        #extra_kwargs = {
+        #    'url': {'view_name': 'trial-detail-uuid', 'lookup_field': 'uuid'}
+        #}
 
     def create(self, validated_data):
         
@@ -176,13 +213,15 @@ class TrialNestedSerializer(serializers.ModelSerializer):
 
         # Save traits
         for trait_data in traits:
+            print("TRAIT ", trait_data)
             # Add project association to trial
             trait_data['trial'] = trial.id
-            new_trait = TraitNestedSerializer(data = trait_data)
-            if new_trait.is_valid():
-                new_trait.save()
+            trait_serializer = TraitNestedSerializer(data = trait_data)
+            if trait_serializer.is_valid():
+                trait = trait_serializer.save()
+                print("TRAITSAVED ",trait)
                 # .add() should make use of Trial model many-to-many Traits through TrialTraits
-                trial.traits.add(new_trait)
+                trialtrait = fpmodels.TrialTrait.objects.create(trial=trial,trait=trait)
             else:
                 logger.debug("trial data is not valid")
 
@@ -196,7 +235,6 @@ class TrialNestedSerializer(serializers.ModelSerializer):
         
         instance.uuid = validated_data.get('uuid', instance.uuid)
         return instance
-
 
 class ProjectSerializer(serializers.ModelSerializer):
 
@@ -241,12 +279,6 @@ class ProjectNestedSerializer(serializers.ModelSerializer):
                 logger.debug("ProjectNestedSerializer: trial is not valid")
        
         return project
-
-class TraitSerializer(serializers.HyperlinkedModelSerializer):
-
-    class Meta:
-        model = fpmodels.Trait
-        fields = ("url","caption", "description", "data_type", "uuid")
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
 
