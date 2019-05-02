c72dc927af559f19af3e3fa99ad4893043d3230a
Tim Erwin <erw001@snivy-cl.nexus.csiro.au>
2019-05-03 09:56:36 +1000

    Modified import to get everything

--- fieldprime-api/fpapi/models/__init__.py
+++ fieldprime-api/fpapi/models/__init__.py
@@ -2,6 +2,6 @@ from .attributevalue import AttributeValue
 from .datum import Datum
 from .devicename import DeviceName
 from .system import System
-from .trialtrait import Trial, Trait, TrialTrait, Token, Node, NodeAttribute, TrialTraitNumeric, TraitCategory
+from .trialtrait import *
 from .tokennode import TokenNode
 from .userproject import User, Project
