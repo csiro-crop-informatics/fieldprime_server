02cb0d52b548ca65b4cccbea87aa38e3a8d79526
Tim Erwin <erw001@snivy-cl.nexus.csiro.au>
2019-05-06 12:19:03 +1000

    Add datum path

--- fieldprime-api/fpapi/urls.py
+++ fieldprime-api/fpapi/urls.py
@@ -10,7 +10,6 @@ router.register(r'projects', fpview.ProjectNestedViewSet)
 #router.register(r'traits', fpview.TraitViewSet)
 #router.register(r'nodes', fpview.NodeViewSet)
 
-
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 
@@ -25,14 +24,12 @@ urlpatterns = [
     path('v2/nodes/<uuid:barcode>/', fpview.NodeViewSet.as_view({'get': 'retrieve'}), name='node-detail-id'),
 
     # Trait Views
-    path('v2/trials/<uuid:uuid>/traits', fpview.TraitListByTrial.as_view(), name='list-trial-traits'),
+    path('v2/trials/<uuid:uuid>/traits/', fpview.TraitListByTrial.as_view(), name='list-trial-traits'),
     #path('v2/trials/<uuid:uuid>/traits', fpview.TraitUpdateByTrial.as_view(), name='update-trial-traits'),
     
     # Datum Views
-    path('v2/trials/<uuid:uuid>/datum', fpview.DatumListByTrial.as_view(), name='list-trial-datum'),
+    path('v2/trials/<uuid:uuid>/datum/', fpview.DatumListByTrial.as_view(), name='list-trial-datum'),
     
     path('v2/', include(router.urls)),
-    #path('v2/trials/<uuid:uuid>/',fpview.TrialViewSet.as_view({'get': 'list'})),
-    #path('projects/<int:project_id>/users/', fpview.ProjectMemberList.as_view()),
 
 ]
