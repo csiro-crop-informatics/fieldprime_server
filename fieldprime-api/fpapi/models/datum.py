62272bab539571ccc323bc5863568c1b42f14cd4
Tim Erwin <erw001@snivy-cl.nexus.csiro.au>
2019-05-06 12:10:09 +1000

    Code cleanup and remove old ids (merge ids)

--- fieldprime-api/fpapi/models/datum.py
+++ fieldprime-api/fpapi/models/datum.py
@@ -5,33 +5,58 @@ from .trialtrait import Node, TraitInstance
 
 class Datum(models.Model):
 
+    node = models.ForeignKey(
+        Node,
+        on_delete = models.CASCADE,
+    )
+    trait_instance = models.ForeignKey(
+        TraitInstance,
+        on_delete = models.CASCADE,
+        db_column ='traitInstance_id'
+    )
+    
+    user_id = models.TextField(
+        db_column = 'userid',
+        blank = True,
+        null = True
+    )
+
+    timestamp = models.BigIntegerField()
+    gps_long = models.FloatField(
+        blank = True,
+        null = True
+    )
+    gps_lat = models.FloatField(
+        blank = True,
+        null = True
+    )
+    notes = models.TextField(
+        blank = True,
+        null= True
+    )
+    num_value = models.DecimalField(
+        db_column = 'numValue',
+        max_digits = 11,
+        decimal_places = 3,
+        blank = True,
+        null = True
+    )
+    txt_value = models.TextField(
+        db_column = 'txtValue',
+        blank = True,
+        null = True
+    )
+
+    """
+    # Historical data was contained in separate
+    # databases, here we store their old ids
+    # No longer used this was from a previous merge attempt.
     project = models.ForeignKey(
         Project,
         on_delete=models.CASCADE,
         blank=True, 
         null=True,
         )
-    node = models.ForeignKey(
-        Node,
-        on_delete=models.CASCADE,
-        )
-    trait_instance = models.ForeignKey(
-        TraitInstance,
-        on_delete=models.CASCADE,
-        db_column='traitInstance_id'
-        )
-    
-    user_id = models.TextField(db_column='userid', blank=True, null=True)
-
-    timestamp = models.BigIntegerField()
-    gps_long = models.FloatField(blank=True, null=True)
-    gps_lat = models.FloatField(blank=True, null=True)
-    notes = models.TextField(blank=True, null=True)
-    num_value = models.DecimalField(db_column='numValue', max_digits=11, decimal_places=3, blank=True, null=True)  # Field name made lowercase.
-    txt_value = models.TextField(db_column='txtValue', blank=True, null=True)  # Field name made lowercase.
-
-    # Historical data was contained in separate
-    # databases, here we store their old ids
     migrated_node = models.IntegerField(
         null=True, 
         blank=True,
@@ -42,13 +67,21 @@ class Datum(models.Model):
         blank=True,
         db_column = 'old_traitInstance_id',
     )
+    """
 
     @property
-    def get_trait_id(self):
+    def get_node_uuid(self):
         """
         Used in datum serializer
         """
-        return self.trait_instance.trait.id
+        return self.node.barcode
+    
+    @property
+    def get_trait_uuid(self):
+        """
+        Used in datum serializer
+        """
+        return self.trait_instance.trait.uuid
 
     class Meta:
         db_table = 'datum'
