c8b9f28d05bf2f37d47a39b2ebb10d97b2005906
Tim Erwin <erw001@snivy-cl.nexus.csiro.au>
2019-05-06 12:11:31 +1000

    Code cleanup

--- fieldprime-api/fpapi/models/devicename.py
+++ fieldprime-api/fpapi/models/devicename.py
@@ -3,13 +3,17 @@ from .userproject import Project
 
 class DeviceName(models.Model):
 
-    project = models.ForeignKey(
-        Project,
-        on_delete=models.CASCADE
-        )
+    android_id = models.CharField(
+        db_column = 'androidId',
+        max_length = 16
+    )
 
-    android_id = models.CharField(db_column='androidId', max_length=64)  # Field name made lowercase.
-    nickname = models.CharField(db_column='nickName', max_length=255, blank=True, null=True)  # Field name made lowercase.
+    nickname = models.CharField(
+        db_column = 'nickName',
+        max_length = 63,
+        blank = True,
+        null = True
+    )
 
     class Meta:
         db_table = 'deviceName'
